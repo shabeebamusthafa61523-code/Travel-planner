0b2381dffb3dc2934a73bbd2b95bea68616320b9
shabeebamusthafa61523-code <shabeebamusthafa61523@gmail.com>
2026-04-01 15:02:18 +0530

    mongo2

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // Use VITE_ prefix for Vite environment variables
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
